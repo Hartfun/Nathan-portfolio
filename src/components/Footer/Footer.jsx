6fb0c748a2b7fb5a5f55d8003db286068321149c
Nathan Furtado <nigelfurtado90@gmail.com>
2025-08-29 12:39:57 +0530

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,21 +10,18 @@ function Footer() {
       <div className="footer-top">
           <div className="footer-top-left">
               <img src={footer_logo} alt="" />
-              <p>I am a Data Aalyst from India, with a fresh beginging into Data Analysis & Data science</p>
-          </div>
+              <p>I am a Data Analyst from India, with a fresh beginning into Data Analysis & Data Science.</p>
+            </div>
           <div className="footer-top-right">
               <div className="footer-email-input">
-                  <img src={user_icon} alt="" />
-                  <input type="email" placeholder="Enter your email" />
               </div>
-              <div className="footer-subscribe">Subscribe</div>
           </div>
       </div>
       <hr />
       <div className="footer-bottom">
           <p className='footer-bottom-left'>© 2025 Nathan Furtado. All rights reserved </p>
           <div className="footer-bottom-right">
-              <p>Terms of Servive</p>
+              <p>Terms of Service</p>
               <p>Privacy Policy</p>
               <p>Connect with me</p>
           </div>
